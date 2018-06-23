999aaebb697c1f135aa7968d2bc59c55bcd32870
amberraza <amberraza.cs@gmail.com>
2018-06-23 11:30:12 -0400

    accessibility + more styles

--- js/main.js
+++ js/main.js
@@ -161,6 +161,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.alt = 'Restaurant Image for '+restaurant.name;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -88,6 +88,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
+  image.alt = 'Restaurant Image for '+restaurant.name;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
